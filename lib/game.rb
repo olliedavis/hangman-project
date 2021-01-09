89c9ce29360b1c78b155a95c40ad00c19f6bc8ff
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-01-09 17:25:33 +0000

    Introduction Cleanup

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,17 @@
 class Hangman
+  def  self.introduction
+    puts ""
+    puts "Welcome to my Hangman Game."
+    puts ""
+    puts "The rules are simple:"
+    puts ""
+    puts "The PC will pick a secret word at random"
+    puts "Your job is to decipher what the secret word is by guessing each character one at a time."
+    puts "If you guess incorrectly, you lose one of your 5 lives."
+    puts "If you know want to take a guess at the word, just type 'ANSWER' to enter guessing mode."
+    puts "If you reveal the word without dying, you win!"
+  end
+
   private
   def word_pick
     valid_words = []
@@ -6,7 +19,9 @@ class Hangman
       if word.length >= 5 and word.length <= 12
         valid_words.push word
       end
-    end
-    word_choice = valid_words[rand(0..valid_words.length)]
+    end 
+    valid_words[rand(0..valid_words.length)]
   end
 end
+
+Hangman.introduction
