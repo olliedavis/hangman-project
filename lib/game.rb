d4fba6065af419cd24842ee65ad3759dc3958f23
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-01-09 17:13:03 +0000

    Reads from word TXT and picks one word at random

---
+++ lib/game.rb
@@ -0,0 +1,12 @@
+class Hangman
+  private
+  def self.word_pick
+    valid_words = []
+    File.foreach("../5desk.txt") do |word|
+      if word.length >= 5 and word.length <= 12
+        valid_words.push word
+      end
+    end
+    word_choice = valid_words[rand(0..valid_words.length)]
+  end
+end
